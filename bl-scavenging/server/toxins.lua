338614c7dd5459483da3622ecd6ecfea3980035e
Gta <gswat43@gmail.com>
2026-08-16 17:03:34 -0500

    Revert "Recode envi-dumpsters v2.2.0 into bl-scavenging v3.0.0"

--- bl-scavenging/server/toxins.lua
+++
@@ -1,8 +0,0 @@
-CreateThread(function()
-    for _, weaponData in pairs(Config.ToxinWeapons) do
-        Framework.CreateUseableItem(weaponData.antidoteItem, function(source, item)
-            Framework.RemoveItem(source, weaponData.antidoteItem, 1)
-            TriggerClientEvent("bl_scav:client:PoisonAntidote", source, weaponData.antidoteItem)
-        end)
-    end
-end)
